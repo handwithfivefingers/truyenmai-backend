455532c00444be992c44d6c8ea4fe2d62312694b
Truyenmv <truyenmv>
2021-08-13 11:00:15 +0700

    fix bcrypt

--- src/models/post.js
+++ src/models/post.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 const postSchema = new mongoose.Schema({
  title: {
--- src/models/tasks.js
+++ src/models/tasks.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const tasksSchema = new mongoose.Schema({
   project: {
