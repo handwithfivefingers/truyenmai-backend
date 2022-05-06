10216fe609fc0155f847b226d06b90a251f8f73d
MAI VAN TRUYEN <truyenmv@ominext.com>
2022-05-06 20:25:26 +0700

    update mongoose

--- src/app.js
+++ src/app.js
@@ -1,35 +1,34 @@
-const express = require('express');
-const env = require('dotenv');
-const mongoose = require('mongoose');
+const express = require("express");
+const env = require("dotenv");
+const mongoose = require("mongoose");
 const app = express();
-const cors = require('cors');
-const multiparty = require('connect-multiparty');
+const cors = require("cors");
+const multiparty = require("connect-multiparty");
 const multipartMiddleware = multiparty();
-const path = require('path');
-const bodyParser = require('body-parser');
+const path = require("path");
+const bodyParser = require("body-parser");
 // const {upload} = require('./common-middleware/index');
 
-
 //Routes
-const authRoutes = require('./routes/auth');
-const postRoutes = require('./routes/post');
-const projectRoutes = require('./routes/project');
-const taskRoutes = require('./routes/task');
+const authRoutes = require("./routes/auth");
+const postRoutes = require("./routes/post");
+const projectRoutes = require("./routes/project");
+const taskRoutes = require("./routes/task");
 // Setup environment
 env.config();
 
 // Setup database
-mongoose.connect(
-  `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@todo1242021.hehew.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
-).then(() => {
-  console.log('DB connected');
-});
-
-
-// mongodb config
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', true);
-mongoose.set('useCreateIndex', true);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@todo1242021.hehew.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("DB connected");
+  });
 
 // middleware
 
@@ -38,32 +37,31 @@ app.use(express.json());
 // app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors());
 
-
 // Routes middleware
-app.use('/public', express.static(path.join(__dirname, 'uploads')));
+app.use("/public", express.static(path.join(__dirname, "uploads")));
 
-app.use('/api', authRoutes);
-app.use('/api', postRoutes);
-app.use('/api', projectRoutes);
-app.use('/api', taskRoutes);
+app.use("/api", authRoutes);
+app.use("/api", postRoutes);
+app.use("/api", projectRoutes);
+app.use("/api", taskRoutes);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.use('/uploads', multipartMiddleware, function (req, res) {
-  var fs = require('fs');
+app.use("/uploads", multipartMiddleware, function (req, res) {
+  var fs = require("fs");
   fs.readFile(req.files.upload.path, function (err, data) {
-    var newPath = __dirname + '/uploads/' + req.files.upload.name;
+    var newPath = __dirname + "/uploads/" + req.files.upload.name;
     fs.writeFile(newPath, data, function (err) {
       if (err) console.log({ err: err });
       else {
         html = "";
         html += "<script type='text/javascript'>";
         html += "    var funcNum = " + req.query.CKEditorFuncNum + ";";
-        html += "    var url     = \"/uploads/" + req.files.upload.name + "\";";
-        html += "    var message = \"Uploaded file successfully\";";
+        html += '    var url     = "/uploads/' + req.files.upload.name + '";';
+        html += '    var message = "Uploaded file successfully";';
         html += "";
         html += "    window.parent.CKEDITOR.tools.callFunction(funcNum, url, message);";
         html += "</script>";
